0bb05295d4ce31bd5c10da81fcfdcd1415f24874
VincentOnfray <33415283+VincentOnfray@users.noreply.github.com>
2017-11-14 13:33:02 +0100

    Add files via upload

---
+++ Duino_1.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+ 
+  Serial.begin(9600);
+  
+
+}
+
+void loop() {
+  int n=0; 
+  int time;
+  int dtime;
+  time = millis();
+  while (time<5000)
+  {
+    
+  
+ 
+    Serial.println(analogRead(0));
+    n=n+1;
+  
+ 
+  // put your main code here, to run repeatedly:
+  }
+    Serial.println(n);
+}
