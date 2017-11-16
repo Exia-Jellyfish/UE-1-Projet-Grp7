db32e2c57473f651dc27002475dee99ab4943b67
Exia-Jellyfish <33414884+Exia-Jellyfish@users.noreply.github.com>
2017-11-16 10:54:24 +0100

    last1

--- Vrai Coeur C (module 2)/GenerationCode.c
+++ Vrai Coeur C (module 2)/GenerationCode.c
@@ -5,29 +5,28 @@
     void generateHeader(int *modeAffichage, int *nbLedsAllummees, int *ledAllumee)
     {
 
-        FILE fout = NULL;
+        FILE* fout = NULL;
         fout = fopen("param.h", "w");   //param.h est un fichier txt qui sera modifié par le programme pour que l'Arduino reconnaisse le mode d'allumage de l'Arduino.
-        switch (modeAffichage)
+        switch (*modeAffichage)
     {
             case 0:
-                fprintf(fout, "Le mode d'affichage est le %d; soit clignotement du coeur\n", *modeAffichage);
+                fprintf(fout, "int modeAffichage = %d;\n", *modeAffichage);
             break;
 
             case 1:
-                fprintf(fout, "Le mode d'affichage est le %d; soit 1 Led sur X\nX a pour valeur : %d;\n", *modeAffichage, *nbLedsAllummees);
+                fprintf(fout, "int modeAffichage = %d;\nint nbLedsAllumees = %d;\n", *modeAffichage, *nbLedsAllummees);
             break;
 
             case 2:
-                fprintf(fout, "Le mode d'affichage est le %d; soit 1 Led allumee de votre choix (numero %d;)\n", *modeAffichage, *ledAllumee);
+                fprintf(fout, "int modeAffichage = %d;\nint ledAllumee = %d;\n", *modeAffichage, *ledAllumee);
             break;
 
             case 3:
-                fprintf(fout, "Le mode d'affichage est le %d; soit l'allumage en chenille avec %d; Leds à la fois\n", *modeAffichage, *nbLedsAllummees);
+                fprintf(fout, "int modeAffichage = %d;\nint nbLedsAllumees = %d;\n", *modeAffichage, *nbLedsAllummees);
             break;
 
             default:
-
-            break;
+                break;
 
     }
 }
