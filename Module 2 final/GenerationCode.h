4f834211faeb40f396235815a457a4db5327d598
Exia-Jellyfish <33414884+Exia-Jellyfish@users.noreply.github.com>
2017-11-16 20:14:20 +0100

    Module 2 FINAL

--- Module 2 final/GenerationCode.h
+++ Module 2 final/GenerationCode.h
@@ -1,7 +1,7 @@
 #ifndef GENERATIONCODE_H_INCLUDED
 #define GENERATIONCODE_H_INCLUDED
 
-    void generateHeader(int *modeAffichage, int *nbLedsAllummees, int *ledAllumee);
+    void generateHeader(int *modeAffichage, int *nbLedsAllummees, int *LedAllumee);
 
 
 #endif // GENERATIONCODE_H_INCLUDED
